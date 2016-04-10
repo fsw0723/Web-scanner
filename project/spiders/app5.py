a7ef7f79e521b638675f522dbb9a7699cc7fdf09
Shuwei <sfang@thoughtworks.com>
2016-04-11 00:53:49 +0800

    Login

--- project/spiders/app5.py
+++ project/spiders/app5.py
@@ -8,21 +8,36 @@ class App5Spider(scrapy.Spider):
     name = "app5"
     allowed_domains = ["app5.com"]
     start_urls = (
-        'https://app5.com/www/index.php',
+        'https://app5.com/www/index.php?index_page',
     )
 
-    def parse(self, response):
-        # GET requests
-        for href in response.css("a::attr('href')"):
-            url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse_get_requests)
+    # def parse(self, response):
+    #     # GET requests
+    #     print response.request.url
+    #     for href in response.css("a::attr('href')"):
+    #         url = response.urljoin(href.extract())
+    #         yield scrapy.Request(url, callback=self.parse_get_requests)
+    #
+    # def parse_get_requests(self, response):
+    #     item = ProjectItem()
+    #     parsed = urlparse.urlparse(response.url)
+    #     parameters = urlparse.parse_qs(parsed.query)
+    #     for parameter in parameters:
+    #         item['url'] = parsed.path
+    #         item['param'] = parameter
+    #         item['type'] = "GET"
+    #         yield item
 
-    def parse_get_requests(self, response):
-        item = ProjectItem()
-        parsed = urlparse.urlparse(response.url)
-        parameters = urlparse.parse_qs(parsed.query)
-        for parameter in parameters:
-            item['url'] = parsed.path
-            item['param'] = parameter
-            item['type'] = "GET"
-            yield item
+    def parse(self, response):
+        return scrapy.FormRequest.from_response(
+            response,
+            formdata={'login': 'student', 'password': 'student'},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        # check login succeed before going on
+        if "incorrect" in response.body:
+            self.logger.error("Login failed")
+            return
+
