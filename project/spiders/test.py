f7033823b6fb7b479bc50700de8454eb087b8847
Shuwei <sfang@thoughtworks.com>
2016-04-13 01:25:39 +0800

    login before crawl

--- project/spiders/test.py
+++ project/spiders/test.py
@@ -1,3 +1,4 @@
+import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
@@ -6,9 +7,11 @@ import urlparse
 from project.items import ProjectItem
 from scrapy.http import  Request
 import pprint
+from scrapy.contrib.spiders.init import InitSpider
+
 #import sys
 #sys.stdout=open('output.txt','w')
-class TestSpider(CrawlSpider):
+class TestSpider(InitSpider):
     name = "test"
     allowed_domains = ["app5.com"]
     start_urls = ["https://app5.com/www/index.php",
@@ -16,6 +19,8 @@ class TestSpider(CrawlSpider):
     link_extractor = {
         'next_page':  LinkExtractor(allow=(),restrict_css=('a'))
     }
+
+    login_page = "https://app5.com/www/index.php?index_page"
 
     def parse(self, response):
         print "---------------"
@@ -39,11 +44,25 @@ class TestSpider(CrawlSpider):
 
             yield Request(url = link.url, callback=self.parse)
 
+    def init_request(self):
+        print "init request!!!!!!!!!!"
+        """This function is called before crawling starts."""
+        return Request(url=self.login_page, callback=self.login)
 
-    def myparse(self, response):
-        hxs = Selector(response)
-        items =[]
-        item = ProjectItem()
-        item['url'] =  response.url
-        items.append(item)
-        return items
+    def login(self, response):
+        """Generate a login request."""
+        return scrapy.FormRequest.from_response(response,
+                                         formdata={'login': 'student', 'password': 'student'},
+                                         callback=self.check_login_response)
+
+    def check_login_response(self, response):
+        """Check the response returned by a login request to see if we are
+        successfully logged in.
+        """
+        if "incorrect" not in response.body:
+            self.log("Successfully logged in. Let's start crawling!")
+            # Now the crawling can begin..
+            return self.initialized()
+        else:
+            self.log("Bad times :(")
+            # Something went wrong, we couldn't log in, so nothing happens.
